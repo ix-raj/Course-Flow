a897b788e037b1eb20335488a8481f60fa7151b1
Ankit Raj <ankitraj5024@gmail.com>
2026-04-04 00:48:36 +0530

    fix the middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,7 +6,7 @@ const protect = async (req, res, next) => {
 
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
-      token = req.headers.authorization.split(' ');
+      token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       // Find the user in the database
