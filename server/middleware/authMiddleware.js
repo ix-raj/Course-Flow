1505e2eca9ce1a7d5bc176238f3ec618114ad8e8
Ankit Raj <ankitraj5024@gmail.com>
2026-04-03 10:59:25 +0530

    Fix AuthMiddleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,7 +6,8 @@ const protect = async (req, res, next) => {
 
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
-      token = req.headers.authorization.split(' ');
+      // ADD THE HERE:
+      token = req.headers.authorization.split(' ')[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
